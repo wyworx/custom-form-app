719071725bc00bb6236c21a3c9820e902e39d44d
murat-polat <muratsdepo@gmail.com>
2020-12-27 20:12:53 +0000

    cascade-on-delete

--- custom_reg_form/models.py
+++ custom_reg_form/models.py
@@ -10,7 +10,7 @@ class ExtraInfo(models.Model):
     This model contains two extra fields that will be saved when a user registers.
     The form that wraps this model is in the forms.py file.
     """
-    user = models.OneToOneField(USER_MODEL, null=True)
+    user = models.OneToOneField(USER_MODEL, null=True,  related_name='user+', on_delete=models.CASCADE)
     
     phone_number = models.CharField(
         verbose_name="Phone Number",
