399cb88a7cbecc6dd03a1d428b5340ae7887d755
Nadheem Abdulla <63186421+nadheemabdulla@users.noreply.github.com>
2020-12-29 17:39:02 +0530

    Update admin.py

--- custom_reg_form/admin.py
+++ custom_reg_form/admin.py
@@ -7,18 +7,18 @@ from io import StringIO
 
 class ExtraInfoAdmin(admin.ModelAdmin):
     actions = ['download_csv'] 
-    list_display = ('user', 'phone_number',)
-    list_display_links = ('user', 'phone_number',)
+    list_display = ('user', 'nationality', 'age', 'phone_number',)
+    list_display_links = ('user', 'nationality', 'age', 'phone_number',)
     list_filter = ('user',)
-    search_fields = ('user', 'phone_number',)
+    search_fields = ('user', 'nationality', 'age', 'phone_number',)
     list_per_page = 25  
     def download_csv(self, request, queryset,*args, **kwargs):
         import csv
         f = open('some.csv', 'w')
         writer = csv.writer(f)
-        writer.writerow(['user', 'phone_number',])
+        writer.writerow(['user', 'nationality', 'age', 'phone_number',])
         for s in queryset:
-            writer.writerow([s.user, s.phone_number])
+            writer.writerow([s.user, s.nationality, s.age, s.phone_number])
         
         f.close()
         f = open('some.csv', 'r')
