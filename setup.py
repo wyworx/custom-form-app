1d8489a8e714e2f69a9a4da6e0fd0455926f55b0
wyworx <65986007+wyworx@users.noreply.github.com>
2021-03-03 22:07:44 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup
 
 setup(
     name='custom-form-app',
-    version='1.0',
+    version='1.1',
     description='LMS - Custom Registration Extension Form',
     packages=['custom_reg_form'],
     install_requires=[
